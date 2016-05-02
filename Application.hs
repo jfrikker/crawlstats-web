8b8b692367a9a9f5016b27e59c6ab985bcefdb23
Joe Frikker <jfrikker@gmail.com>
2016-05-01 20:53:39 -0400

    Removing scaffold routes

--- Application.hs
+++ Application.hs
@@ -33,8 +33,6 @@ import qualified Crawl.Stats.Data as CrawlData
 -- Don't forget to add new modules to your cabal file!
 import Handler.Common
 import Handler.Crawl
-import Handler.Home
-import Handler.Comment
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
